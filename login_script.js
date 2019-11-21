7f3d6a891fdcb36937de1720e579d297c00b4042
Brooke <brookebordona@gmail.com>
2019-11-21 16:48:42 -0500

    login_script setup

---
+++ login_script.js
@@ -0,0 +1,16 @@
+
+async function login() {
+    
+}
+
+async function createAccount() {
+
+}
+
+const loadPage = function() {
+    //$("#feed").on("click", ".like", handleLikeTweet);
+}
+
+$(function () {
+    loadPage();
+});
