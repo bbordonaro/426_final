7bc6a5b246539799137b77093c710005d42629ee
Brooke <brookebordona@gmail.com>
2019-11-21 18:11:50 -0500

    create & partial login requests

--- login_script.js
+++ login_script.js
@@ -1,14 +1,47 @@
-
 async function login() {
+    const $form = $('#login-form');
+    const data = $form.serializeArray().reduce((o, x) => {
+            o[x.name] = x.value;
+            return o;
+        }, {});
+
+    //console.log("result");
+
+    const result = await axios({
+        method: 'post',
+        url: 'http://localhost:3000/account/login',
+        data: {
+            "name": data.name,
+            "pass": data.password
+        }
+    });
     
 }
 
 async function createAccount() {
+    const $form = $('#create-form');
+    const data = $form.serializeArray().reduce((o, x) => {
+            o[x.name] = x.value;
+            return o;
+        }, {});
 
+    const result = await axios({
+        method: 'post',
+        url: 'http://localhost:3000/account/create',
+        data: {
+            "name": data.name,
+            "pass": data.password
+        }
+    });
+
+    result.then(function() {
+        console.log("Hello");
+    })
 }
 
-const loadPage = function() {
-    //$("#feed").on("click", ".like", handleLikeTweet);
+const loadPage = function () {
+    $('.create_button').on("click", createAccount);
+    $('.login_button').on("click", login);
 }
 
 $(function () {
