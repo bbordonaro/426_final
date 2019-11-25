62b18731aa8785a1bdbf6cef1b95b14d6adf36e0
hannabee <hannabee@hannabee-pc.wireless-1x.unc.edu>
2019-11-25 18:11:59 -0500

    rearranged folders

---
+++ comp426frontend/questions.js
@@ -0,0 +1,81 @@
+const quiz = [{
+    question: "How do you like to spend your free time?",
+    answers: {
+        a: "Relaxing with wine and a new book",
+        b: "Shopping some local street markets",
+        c: "Hiking the closest peaks",
+        d: "Seeing a movie with a large popcorn"
+    }
+}, {
+    question: "Which season is your favorite?",
+    answers: {
+        a: "Spring",
+        b: "Summer",
+        c: "Fall",
+        d: "Winter"
+    }
+}, {
+    question: "Which snack would slap right now?",
+    answers: {
+        a: "A burrito",
+        b: "French Fries",
+        c: "Cheese and Fruit",
+        d: "Chocolate"
+    }
+}, {
+    question: "Which movie series could you binge on repeat?",
+    answers: {
+        a: "Harry Potter",
+        b: "Cheetah Girls",
+        c: "Sisterhood of the Travelling Pants",
+        d: "Jurassic Park"
+    }
+}, {
+    question: "What were the best memes of 2019?",
+    answers: {
+        a: "Storming Area 51",
+        b: "Rise and Shine",
+        c: "Trying Kombucha for the First Time",
+        d: "Let Me INNNNNN"
+    }
+}, {
+    question: "Pick an activity.",
+    answers: {
+        a: "Surfing",
+        b: "Spa Day",
+        c: "Horseback Riding",
+        d: "Museum-ing"
+    }
+}, {
+    question: "Choose your favorite artist of 2019.",
+    answers: {
+        a: "Ariana Grande",
+        b: "Khalid",
+        c: "Travis Scott",
+        d: "Billie Eilish"
+    }
+}, {
+    question: "Where do you want to stay on your trip?",
+    answers: {
+        a: "AirBnb",
+        b: "Hotel",
+        c: "Hostel",
+        d: "I'll figure it out"
+    }
+}, {
+    question: "Which would be the best story to tell after you get home?",
+    answers: {
+        a: "Getting into a bar fight",
+        b: "Falling in love",
+        c: "Taking a sick insta pic",
+        d: "Learning a new skill"
+    }
+}, {
+    question: "How many people do you want to travel with?",
+    answers: {
+        a: "Me, Myself, and I",
+        b: "1 or 2",
+        c: "The whole squad",
+        d: "Everyone I know - I have fomo"
+    }
+}];
