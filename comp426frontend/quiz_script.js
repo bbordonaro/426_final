00a59e6780e3bd80d8d02698a42e4166093b9436
Brooke <brookebordona@gmail.com>
2019-12-05 16:19:09 -0500

    quiz questions & functionality working

--- comp426frontend/quiz_script.js
+++ comp426frontend/quiz_script.js
@@ -1,39 +1,45 @@
-document.getElementById("form1").onsubmit=function() {
-
-
-    // age = parseInt(document.querySelector('input[name = "age"]:checked').value);
-
-    // activity = parseInt(document.querySelector('input[name = "activity"]:checked').value);
-
-    // genre = parseInt(document.querySelector('input[name = "genre"]:checked').value);
-
-    // total= age + activity + genre;
-
-
-    // document.getElementById("answer").innerHTML = total;
-
-    // if(total < 4) {
-    //     document.getElementById("answer2").innerHTML = "You got Quiet Company.";
-    // }
-
-    // if(total >=4 && total < 7) {
-    //     document.getElementById("answer2").innerHTML = "You got Spoon.";
-    // }
-
-    // if(total >=7) {
-    //     document.getElementById("answer2").innerHTML = "You got Willie Nelson.";
-    // }
-
-    return false; // required to not refresh the page; leave this here
-}
-
 const renderQuestion = function(q) {
     return `
     <div class="question"><p>${q.question}</p>
-    <div class="option a">${q.answers.a}</div>
-    <div class="option b">${q.answers.b}</div>
-    <div class="option c">${q.answers.c}</div>
-    <div class="option d">${q.answers.d}</div></div>`;
+    <div class="option a ${q.id}">${q.answers.a}</div>
+    <div class="option b ${q.id}">${q.answers.b}</div>
+    <div class="option c ${q.id}">${q.answers.c}</div>
+    <div class="option d ${q.id}">${q.answers.d}</div></div>`;
+};
+
+const getResults = function(choices){
+    //count number of each letter
+    var counts = {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0
+    }
+    for(let i=1; i<11; i++){
+        let ans = choices[i];
+        if(ans == "a"){
+            counts.a++;
+        } else if(ans == "b"){
+            counts.b++;
+        } else if(ans == "c"){
+            counts.c++;
+        } else if(ans == "d"){
+            counts.d++;
+        }
+    }
+    var result = Object.keys(counts).reduce((a, b) => counts[a] > counts[b] ? a : b);
+    var place;
+    if(result == "a") {
+        place = results[0];
+    } else if(result == "b") {
+        place = results[1];
+    } else if(result == "c") {
+        place = results[2];
+    } else {
+        place = results[3];
+    }
+    return place;
+    
 };
 
 $(function() {
@@ -44,9 +50,51 @@ $(function() {
         form.append(temp);
     }
 
-    $('.option').on("click", function() {
+    var choices = {
+        1: "z",
+        2: "z",
+        3: "z",
+        4: "z",
+        5: "z",
+        6: "z",
+        7: "z",
+        8: "z",
+        9: "z",
+        10: "z",
+    };
 
+    $('.option').on("click", function() {
+        var choice = event.target;
+        var classes = choice.className.split(/\s+/);
+        var question_clicked = classes[2];
+        var letter = classes[1];
+        var question = quiz[question_clicked-1];
+        choices[question_clicked] = letter;
+        var options = document.getElementsByClassName(question_clicked);
+        for(let i=0; i<4; i++) {
+            let x = options[i];
+            let temp = x.className.split(/\s+/)[1];
+            if(temp != letter){
+                x.style.backgroundColor = "lightblue";
+            } else {
+                x.style.backgroundColor = "grey";
+            }
+        }
+        
     });
 
-    form.append(`<button type="submit" value="Submit">Submit</button>`);
+    form.append(`<div id = "quiz_submit">Submit</div>`);
+    $("#quiz_submit").on("click", function() {
+        var count = 0;
+        for(var question in choices) {
+            if(choices[question] == "z"){
+                count++;
+            }
+        }
+        if(count != 0){
+            $("#main").append(`You need to answer ${count} more questions!`);
+        } else {
+            getResults(choices);
+        }
+    });
 });
---
+++ comp426frontend/results.js
@@ -0,0 +1,113 @@
+const results = [{
+    category: "Tropical",
+    places: {
+        1: {
+            name: "Maldives",
+            description: "The Maldives are a slow paced, luxurious range of islands with lots of water activities and untouched scenery.",
+            point: {
+                lat: 3.2028,
+                long: 73.2207
+            }
+        },  
+        2: {
+            name: "Kaua'i, Hawaii, U.S.A",
+            description: "This island of Hawaii is covered by a tropical rainforest teeming with wildlife and beautiful plants to discover.",
+            point: {
+                lat: 22.0964,
+                long: -159.5261
+            }
+        }, 
+        3: {
+            name: "Port Douglas, Australia",
+            description: "A base for visits to the Great Barrier Reef, this city also is next to the tropical rainforest of Daintree National Park, so you can get your land and water fix.",
+            point: {
+                lat: -16.4836,
+                long: 145.4653
+            }
+        } 
+    }
+}, {
+    category: "Adventurous",
+    places: {
+        1: {
+            name: "Moab, Utah, U.S.A.",
+            description: "Featuring massive mesas and red rocks, this area sports some of the most extreme mountain biking trails anywhere.",
+            point: {
+                lat: 38.5733,
+                long: -109.5498
+            }
+        },  
+        2: {
+            name: "Patagonia, South America",
+            description: "This mountain range hosts arid steppes, grasslands, deserts, glacial fjords and a temperate rainforest, perfect for any multifaceted explorer.",
+            point: {
+                lat: -41.8101,
+                long: -68.9063
+            }
+        }, 
+        3: {
+            name: "Iceland",
+            description: "Visit geothermal pools, fijords, glaciers, and volcanoes on this huge island in the Atlantic.",
+            point: {
+                lat: 64.9631,
+                long: -19.0208
+            }
+        } 
+    }
+}, {
+    category: "Historical",
+    places: {
+        1: {
+            name: "Greece",
+            description: "The home of democracy, Greecian cities feature many relics and ruins of an ancient civilization that we learned about in mythology.",
+            point: {
+                lat: 37.9838,
+                long: 23.7275
+            }
+        },  
+        2: {
+            name: "Petra, Jordan",
+            description: "This wonder of the world, featured in Indiana Jones, is a magnificent rock carving turned temple in a desert canyon.",
+            point: {
+                lat: 30.328960,
+                long: 35.444832
+            }
+        }, 
+        3: {
+            name: "Machu Picchu, Peru",
+            description: "These ruins of the Incans serve as a reminder of the extensive cultivation plans and technology of this ancient group.",
+            point: {
+                lat: -13.1631,
+                long: -72.5450
+            }
+        } 
+    }
+}, {
+    category: "Food Focused",
+    places: {
+        1: {
+            name: "London, England",
+            description: "As the capital of a power that once spanned the globe, the melting pot of food in London showcases an array of amazing cuisines from many cultures.",
+            point: {
+                lat: 51.5074,
+                long: -0.1278
+            }
+        },  
+        2: {
+            name: "Tuscany, Italy",
+            description: "Of course, the Italian countryside hosts fabulous vineyards and simple, yet fantastic made-from-scratch classics that can't be replicated by just anyone.",
+            point: {
+                lat: 43.7711,
+                long: 11.2486
+            }
+        }, 
+        3: {
+            name: "Delhi, India",
+            description: "Teeming with street food and classic restaurants, this Indian city houses crazy flavors and spices you can't find together anywhere else.",
+            point: {
+                lat: 28.7041,
+                long: 77.1025
+            }
+        } 
+    }
+}]
