79b769e79d33b04bcda5d2973cf17dfce6e5573e
hannabee <hannabee@hannabee-pc.wireless-1x.unc.edu>
2019-12-11 13:21:57 -0500

    final styling etc

--- comp426frontend/script.js
+++ comp426frontend/script.js
@@ -22,7 +22,7 @@ async function initMap() {
                 }
             }).then(y => {
                 let content1 =
-                    '<div class="markercontent">' + y.data.result.places[1].name + "<p class='markerP'>Click this blue marker again for more info!</p>" +
+                    '<div class="markercontent">' + y.data.result.places[1].name + "<p class='markerP'>Click the blue pin again for more info!</p>" +
                     '</div>';
                 let point1 = y.data.result.places[1].point;
                 let infowindow1 = new google.maps.InfoWindow({
@@ -44,12 +44,19 @@ async function initMap() {
                     marker1.addListener('click', newPage1);
                 }
 
+                infowindow1.addListener('closeclick', windowClose1);
+
+                function windowClose1() {
+                    google.maps.event.clearInstanceListeners(marker1);
+                    marker1.addListener('click', openWindow1);
+                }
+
                 function newPage1() {
                     window.location.href = "search.html?jwt=" + jwt + "&place=" + y.data.result.places[1].name;
                 }
 
                 let content2 =
-                    '<div class="markercontent">' + y.data.result.places[2].name + "<p class='markerP'>Click this blue marker again for more info!</p>" +
+                    '<div class="markercontent">' + y.data.result.places[2].name + "<p class='markerP'>Click the blue pin again for more info!</p>" +
                     '</div>';
                 let point2 = y.data.result.places[2].point;
                 let infowindow2 = new google.maps.InfoWindow({
@@ -71,12 +78,19 @@ async function initMap() {
                     marker2.addListener('click', newPage2);
                 }
 
+                infowindow2.addListener('closeclick', windowClose2);
+
+                function windowClose2() {
+                    google.maps.event.clearInstanceListeners(marker2);
+                    marker2.addListener('click', openWindow2);
+                }
+
                 function newPage2() {
                     window.location.href = "search.html?jwt=" + jwt + "&place=" + y.data.result.places[2].name;
                 }
 
                 let content3 =
-                    '<div class="markercontent">' + y.data.result.places[3].name + "<p class='markerP'>Click this blue marker again for more info!</p>" +
+                    '<div class="markercontent">' + y.data.result.places[3].name + "<p class='markerP'>Click the blue pin again for more info!</p>" +
                     '</div>';
                 let point3 = y.data.result.places[3].point;
                 let infowindow3 = new google.maps.InfoWindow({
@@ -96,6 +110,13 @@ async function initMap() {
                     infowindow3.open(map, marker3);
                     google.maps.event.clearInstanceListeners(marker3);
                     marker3.addListener('click', newPage3);
+                }
+
+                infowindow3.addListener('closeclick', windowClose3);
+
+                function windowClose3() {
+                    google.maps.event.clearInstanceListeners(marker3);
+                    marker3.addListener('click', openWindow3);
                 }
 
                 function newPage3() {
@@ -315,7 +336,7 @@ const loadPage = function () {
                         return b[1] - a[1];
                     });
                     let count = 1;
-                    for (let i = 0; i < 6; i++) {
+                    for (let i = 0; i < 5; i++) {
                         let place = $("<div></div");
                         place.append(count + ". " + placesArray[i][0] + ": ");
                         place.append(placesArray[i][1] + " users");
@@ -336,7 +357,7 @@ const loadPage = function () {
                 method: 'get',
                 url: 'http://localhost:3000/public/users',
             });
-            $("#userCount").append("Join the " + result.data.result + " users who have found great destinations with Hex Girls Travel Agency!");
+            $("#userCount").append("Join the " + "<span id='userNumber'>" + result.data.result + "</span>" + " users who have found great destinations with Hex Girls Travel Agency!");
         } catch (error) {
             $("#userCount").append("No users yet, be our first and create an account!");
         }
